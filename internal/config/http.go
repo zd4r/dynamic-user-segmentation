66b8e9c11dce8e775973ac22c73bae5730fc4594
zd4rova <fakenotbad75@gmail.com>
2023-08-30 00:25:19 +0300

    add: http config

--- internal/config/http.go
+++ internal/config/http.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"errors"
+	"os"
+)
+
+const (
+	httpPortEnvName = "HTTP_PORT"
+)
+
+type HTTPConfig interface {
+	Port() string
+}
+
+var _ HTTPConfig = (*httpConfig)(nil)
+
+type httpConfig struct {
+	port string
+}
+
+func NewHTTPConfig() (*httpConfig, error) {
+	port := os.Getenv(httpPortEnvName)
+	if len(port) == 0 {
+		return nil, errors.New("http host not found")
+	}
+
+	return &httpConfig{
+		port: port,
+	}, nil
+}
+
+func (cfg *httpConfig) Port() string {
+	return cfg.port
+}
