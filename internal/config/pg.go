7e341ae83e618bdb5dd6e52ff085f50980b13f9c
zd4rova <fakenotbad75@gmail.com>
2023-08-30 00:25:36 +0300

    add: pg config

--- internal/config/pg.go
+++ internal/config/pg.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"errors"
+	"os"
+)
+
+const (
+	pgDSNEnvName = "PG_DSN"
+)
+
+type PGConfig interface {
+	DSN() string
+}
+
+var _ PGConfig = (*pgConfig)(nil)
+
+type pgConfig struct {
+	dsn string
+}
+
+func NewPGConfig() (*pgConfig, error) {
+	dsn := os.Getenv(pgDSNEnvName)
+	if len(dsn) == 0 {
+		return nil, errors.New("pg DSN not found")
+	}
+
+	return &pgConfig{
+		dsn: dsn,
+	}, nil
+}
+
+func (cfg *pgConfig) DSN() string {
+	return cfg.dsn
+}
