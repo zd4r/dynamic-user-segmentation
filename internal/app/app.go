ec3e9da373b03d86709ade9a9b41b806e936a289
zd4rova <fakenotbad75@gmail.com>
2023-08-30 00:26:59 +0300

    add: DI-container

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,27 +3,87 @@ package app
 import (
 	"context"
 	"log"
-	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	httpV1 "github.com/zd4r/dynamic-user-segmentation/internal/api/http/v1"
+	"github.com/zd4r/dynamic-user-segmentation/internal/config"
+	"github.com/zd4r/dynamic-user-segmentation/pkg/closer"
+	"github.com/zd4r/dynamic-user-segmentation/pkg/httpserver"
 )
 
 type App struct {
 	serviceProvider *serviceProvider
 
-	httpServer *http.Server
+	httpServer *httpserver.Server
 }
 
 func NewApp(ctx context.Context) (*App, error) {
 	var a App
 
-	//err := a.initDeps(ctx)
-	//if err != nil {
-	//	return nil, err
-	//}
+	if err := a.initDeps(ctx); err != nil {
+		return nil, err
+	}
 
 	return &a, nil
 }
 
 func (a *App) Run() error {
-	log.Println("App is running")
+	defer func() {
+		closer.CloseAll()
+		closer.Wait()
+	}()
+
+	a.runHTTPServer()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGTERM, os.Interrupt)
+
+	select {
+	case s := <-interrupt:
+		log.Printf("interrupt signal: %s", s.String())
+	case err := <-a.httpServer.Notify():
+		log.Printf("a.httpServer.Notify error: %s", err.Error())
+	}
+
+	if err := a.httpServer.Shutdown(); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (a *App) initDeps(ctx context.Context) error {
+	inits := []func(context.Context) error{
+		config.Init,
+		a.initServiceProvider,
+		a.initHTTPServer,
+	}
+
+	for _, f := range inits {
+		if err := f(ctx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *App) initServiceProvider(_ context.Context) error {
+	a.serviceProvider = NewServiceProvider()
+
+	return nil
+}
+
+func (a *App) initHTTPServer(_ context.Context) error {
+	handler := httpV1.NewRouter()
+	a.httpServer = httpserver.New(handler, httpserver.Port(a.serviceProvider.GetHTTPConfig().Port()))
+
+	return nil
+}
+
+func (a *App) runHTTPServer() {
+	log.Printf("HTTP server is running on %s\n", a.serviceProvider.GetHTTPConfig().Port())
+	a.httpServer.Start()
 }
--- internal/app/service_provider.go
+++ internal/app/service_provider.go
@@ -1,8 +1,72 @@
 package app
 
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/zd4r/dynamic-user-segmentation/internal/client/pg"
+	"github.com/zd4r/dynamic-user-segmentation/internal/config"
+	"github.com/zd4r/dynamic-user-segmentation/pkg/closer"
+)
+
 type serviceProvider struct {
+	pgConfig   config.PGConfig
+	httpConfig config.HTTPConfig
+
+	pgClient pg.Client
 }
 
 func NewServiceProvider() *serviceProvider {
 	return &serviceProvider{}
+}
+
+func (s *serviceProvider) GetPGConfig() config.PGConfig {
+	if s.pgConfig == nil {
+		cfg, err := config.NewPGConfig()
+		if err != nil {
+			log.Fatalf("failed to get pg config: %s", err.Error())
+		}
+
+		s.pgConfig = cfg
+	}
+
+	return s.pgConfig
+}
+
+func (s *serviceProvider) GetHTTPConfig() config.HTTPConfig {
+	if s.httpConfig == nil {
+		cfg, err := config.NewHTTPConfig()
+		if err != nil {
+			log.Fatalf("failed to get http config: %s", err.Error())
+		}
+
+		s.httpConfig = cfg
+	}
+
+	return s.httpConfig
+}
+
+func (s *serviceProvider) GetPGClient(ctx context.Context) pg.Client {
+	if s.pgClient == nil {
+		pgCfg, err := pgxpool.ParseConfig(s.GetPGConfig().DSN())
+		if err != nil {
+			log.Fatalf("failed to parse pg config from dsn: %s", err.Error())
+		}
+
+		pgClient, err := pg.NewClient(ctx, pgCfg)
+		if err != nil {
+			log.Fatalf("failet to get pg client: %s", err.Error())
+		}
+
+		if err := pgClient.PG().Ping(ctx); err != nil {
+			log.Fatalf("failed to ping db: %s", err.Error())
+		}
+
+		closer.Add(pgClient.Close)
+
+		s.pgClient = pgClient
+	}
+
+	return s.pgClient
 }
