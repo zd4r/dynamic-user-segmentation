b1ae1bc3e41442a41cfffae8bd3f44816809f589
zd4rova <fakenotbad75@gmail.com>
2023-08-30 23:20:57 +0300

    update: login from query params

--- internal/api/http/v1/user.go
+++ internal/api/http/v1/user.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	userModel "github.com/zd4r/dynamic-user-segmentation/internal/model/user"
@@ -18,7 +19,7 @@ func newUserRoutes(handler *echo.Group, userService userService) {
 	}
 
 	handler.POST("/user", r.Create)
-	handler.DELETE("/user", r.Delete)
+	handler.DELETE("/user/:login", r.Delete)
 }
 
 type createUserRequest struct {
@@ -35,7 +36,7 @@ func (r *userRoutes) Create(c echo.Context) error {
 	}
 
 	user := &userModel.User{
-		Login: req.Login,
+		Login: strings.ToLower(req.Login),
 	}
 
 	if err := r.userService.Create(ctx, user); err != nil {
@@ -46,21 +47,13 @@ func (r *userRoutes) Create(c echo.Context) error {
 	return c.JSON(http.StatusCreated, nil)
 }
 
-type deleteUserRequest struct {
-	Login string `json:"login"`
-}
-
 func (r *userRoutes) Delete(c echo.Context) error {
 	ctx := c.Request().Context()
 
-	req := new(deleteUserRequest)
-	if err := c.Bind(req); err != nil {
-		log.Println(err) // TODO: logger
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
+	login := strings.ToLower(c.Param("login"))
 
 	user := &userModel.User{
-		Login: req.Login,
+		Login: login,
 	}
 
 	if err := r.userService.Delete(ctx, user); err != nil {
