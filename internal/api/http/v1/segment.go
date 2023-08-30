7ebae99d3f6c47b0aa733895e99a2bf83f294146
zd4rova <fakenotbad75@gmail.com>
2023-08-30 23:20:43 +0300

    update: slug from query params

--- internal/api/http/v1/segment.go
+++ internal/api/http/v1/segment.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	segmentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/segment"
@@ -18,7 +19,7 @@ func newSegmentRoutes(handler *echo.Group, segmentService segmentService) {
 	}
 
 	handler.POST("/segment", r.Create)
-	handler.DELETE("/segment", r.Delete)
+	handler.DELETE("/segment/:slug", r.Delete)
 }
 
 type createSegmentRequest struct {
@@ -35,7 +36,7 @@ func (r *segmentRoutes) Create(c echo.Context) error {
 	}
 
 	segment := &segmentModel.Segment{
-		Slug: req.Slug,
+		Slug: strings.ToLower(req.Slug),
 	}
 
 	if err := r.segmentService.Create(ctx, segment); err != nil {
@@ -46,21 +47,13 @@ func (r *segmentRoutes) Create(c echo.Context) error {
 	return c.JSON(http.StatusCreated, nil)
 }
 
-type deleteSegmentRequest struct {
-	Slug string `json:"slug"`
-}
-
 func (r *segmentRoutes) Delete(c echo.Context) error {
 	ctx := c.Request().Context()
 
-	req := new(deleteSegmentRequest)
-	if err := c.Bind(req); err != nil {
-		log.Println(err) // TODO: logger
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
+	slug := strings.ToLower(c.Param("slug"))
 
 	segment := &segmentModel.Segment{
-		Slug: req.Slug,
+		Slug: slug,
 	}
 
 	if err := r.segmentService.Delete(ctx, segment); err != nil {
