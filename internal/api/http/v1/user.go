8a3a7c187add96b61ce0fa4c417fed604b793a92
zd4rova <fakenotbad75@gmail.com>
2023-08-31 11:30:38 +0300

    add: get user segments route

--- internal/api/http/v1/user.go
+++ internal/api/http/v1/user.go
@@ -7,20 +7,26 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/zd4r/dynamic-user-segmentation/internal/errs"
+	segmentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/segment"
 	userModel "github.com/zd4r/dynamic-user-segmentation/internal/model/user"
 )
 
 type userRoutes struct {
-	userService userService
+	userService       userService
+	experimentService experimentService
 }
 
 func newUserRoutes(handler *echo.Group, userService userService) {
 	r := &userRoutes{
 		userService: userService,
+		//experimentService: experimentService,
 	}
 
 	handler.POST("/user", r.Create)
 	handler.DELETE("/user/:id", r.Delete)
+
+	handler.GET("/user/:id/segments", r.GetSegments)
+	//handler.POST("/user/:id/segments", r.UpdateUserSegments)
 }
 
 type createUserRequest struct {
@@ -41,7 +47,7 @@ func (r *userRoutes) Create(c echo.Context) error {
 	}
 
 	if err := r.userService.Create(ctx, user); err != nil {
-		log.Println(err)
+		log.Println(err) // TODO: logger
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
@@ -66,4 +72,33 @@ func (r *userRoutes) Delete(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, nil)
+}
+
+type getUserSegmentsResponse struct {
+	Segments []segmentModel.Segment `json:"segments"`
+}
+
+func (r *userRoutes) GetSegments(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	userId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, errs.ErrInvalidUserId.Error())
+	}
+
+	user := &userModel.User{
+		Id: userId,
+	}
+
+	segments, err := r.userService.GetSegments(ctx, user)
+	if err != nil {
+		log.Println(err) // TODO: logger
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	resp := getUserSegmentsResponse{
+		Segments: segments,
+	}
+
+	return c.JSON(http.StatusOK, resp)
 }
