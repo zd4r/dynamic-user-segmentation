08f39210a5f8be4d6ced00596107c1881c5574db
zd4rova <fakenotbad75@gmail.com>
2023-08-31 11:30:06 +0300

    update: new service methods

--- internal/api/http/v1/interfaces.go
+++ internal/api/http/v1/interfaces.go
@@ -3,8 +3,11 @@ package v1
 import (
 	"context"
 
+	experimentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/experiment"
 	segmentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/segment"
 	userModel "github.com/zd4r/dynamic-user-segmentation/internal/model/user"
+
+	experimentSrv "github.com/zd4r/dynamic-user-segmentation/internal/service/experiment"
 	segmentSrv "github.com/zd4r/dynamic-user-segmentation/internal/service/segment"
 	userSrv "github.com/zd4r/dynamic-user-segmentation/internal/service/user"
 )
@@ -12,6 +15,7 @@ import (
 type userService interface {
 	Delete(ctx context.Context, user *userModel.User) error
 	Create(ctx context.Context, user *userModel.User) error
+	GetSegments(ctx context.Context, user *userModel.User) ([]segmentModel.Segment, error)
 }
 
 var _ userService = (*userSrv.Service)(nil)
@@ -22,3 +26,10 @@ type segmentService interface {
 }
 
 var _ segmentService = (*segmentSrv.Service)(nil)
+
+type experimentService interface {
+	Delete(ctx context.Context, segment *experimentModel.Experiment) error
+	Create(ctx context.Context, segment *experimentModel.Experiment) error
+}
+
+var _ experimentService = (*experimentSrv.Service)(nil)
