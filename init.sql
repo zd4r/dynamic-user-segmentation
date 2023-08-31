15e10427b4a81df222ac0d6765a648ab4a8ba694
zd4rova <fakenotbad75@gmail.com>
2023-08-31 22:57:38 +0300

    add: expire_at to experiment schema

--- init.sql
+++ init.sql
@@ -13,6 +13,7 @@ CREATE TABLE experiment
 (
     user_id    bigserial,
     segment_id bigserial,
+    expire_at  timestamp(0) with time zone,
     PRIMARY KEY (user_id, segment_id),
     FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE,
     FOREIGN KEY (segment_id) REFERENCES segment(id) ON DELETE CASCADE
