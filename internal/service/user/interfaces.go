4c7ce80dbbce0320cdfe91630bd61205a2666d70
zd4rova <fakenotbad75@gmail.com>
2023-08-31 11:27:46 +0300

    add: GetSegments method

--- internal/service/user/interfaces.go
+++ internal/service/user/interfaces.go
@@ -3,6 +3,7 @@ package user
 import (
 	"context"
 
+	segmentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/segment"
 	userModel "github.com/zd4r/dynamic-user-segmentation/internal/model/user"
 	userRepo "github.com/zd4r/dynamic-user-segmentation/internal/repository/user"
 )
@@ -10,6 +11,7 @@ import (
 type userRepository interface {
 	Create(ctx context.Context, user *userModel.User) error
 	Delete(ctx context.Context, user *userModel.User) (int64, error)
+	GetSegments(ctx context.Context, user *userModel.User) ([]segmentModel.Segment, error)
 }
 
 var _ userRepository = (*userRepo.Repository)(nil)
