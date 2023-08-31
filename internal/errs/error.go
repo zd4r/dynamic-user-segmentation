4e7d55bf4b221b9bceb8a968e76bad17bbcb0abd
zd4rova <fakenotbad75@gmail.com>
2023-09-01 01:07:49 +0300

    add: ErrInvalidExpireAtTime error

--- internal/errs/error.go
+++ internal/errs/error.go
@@ -3,6 +3,7 @@ package errs
 import "errors"
 
 var (
-	ErrNotFound      = errors.New("not found")
-	ErrInvalidUserId = errors.New("invalid user id")
+	ErrNotFound            = errors.New("not found")
+	ErrInvalidUserId       = errors.New("invalid user id")
+	ErrInvalidExpireAtTime = errors.New("invalid expire at time")
 )
