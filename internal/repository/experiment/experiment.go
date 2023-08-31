6115cd371f6ceff4abe6e5c3c7468bfe101947e4
zd4rova <fakenotbad75@gmail.com>
2023-08-31 14:02:51 +0300

    add: CreateBatch and DeleteBatch methods

--- internal/repository/experiment/experiment.go
+++ internal/repository/experiment/experiment.go
@@ -45,6 +45,32 @@ func (r *Repository) Create(ctx context.Context, experiment *experimentModel.Exp
 	return nil
 }
 
+func (r *Repository) CreateBatch(ctx context.Context, experiments []*experimentModel.Experiment) error {
+	builder := sq.Insert(experimentTableName).
+		PlaceholderFormat(sq.Dollar).
+		Columns("user_id", "segment_id")
+
+	for _, experiment := range experiments {
+		builder = builder.Values(experiment.UserId, experiment.SegmentId)
+	}
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return err
+	}
+
+	q := pg.Query{
+		Name:     "experiment.CreateBatch",
+		QueryRaw: query,
+	}
+
+	if _, err := r.client.PG().Exec(ctx, q, args...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (r *Repository) Delete(ctx context.Context, experiment *experimentModel.Experiment) (int64, error) {
 	builder := sq.Delete(experimentTableName).
 		PlaceholderFormat(sq.Dollar).
@@ -58,6 +84,43 @@ func (r *Repository) Delete(ctx context.Context, experiment *experimentModel.Exp
 				},
 			},
 		)
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return 0, err
+	}
+
+	q := pg.Query{
+		Name:     "experiment.Delete",
+		QueryRaw: query,
+	}
+
+	ct, err := r.client.PG().Exec(ctx, q, args...)
+	if err != nil {
+		return 0, err
+	}
+
+	return ct.RowsAffected(), nil
+}
+
+func (r *Repository) DeleteBatch(ctx context.Context, experiments []*experimentModel.Experiment) (int64, error) {
+	var sqOrStatement sq.Or
+	for _, experiment := range experiments {
+		sqOrStatement = append(sqOrStatement,
+			sq.And{
+				sq.Eq{
+					"user_id": experiment.UserId,
+				},
+				sq.Eq{
+					"segment_id": experiment.SegmentId,
+				},
+			},
+		)
+	}
+
+	builder := sq.Delete(experimentTableName).
+		PlaceholderFormat(sq.Dollar).
+		Where(sqOrStatement)
 
 	query, args, err := builder.ToSql()
 	if err != nil {
