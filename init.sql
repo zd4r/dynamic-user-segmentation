61c5908209569be7e09d6dbef54e80d8d5b0b1ae
zd4rova <fakenotbad75@gmail.com>
2023-08-31 20:49:49 +0300

    add: report schema

--- init.sql
+++ init.sql
@@ -16,4 +16,13 @@ CREATE TABLE experiment
     PRIMARY KEY (user_id, segment_id),
     FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE,
     FOREIGN KEY (segment_id) REFERENCES segment(id) ON DELETE CASCADE
+);
+
+CREATE TABLE report
+(
+    id              bigserial                   PRIMARY KEY,
+    user_id         bigserial,
+    segment_slug    text                        NOT NULL,
+    action          text                        NOT NULL,
+    date            timestamp(0) with time zone NOT NULL DEFAULT NOW()
 )
