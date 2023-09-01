ba59e940be802aa60363d77fbf867d17c47dcbf0
zd4rova <fakenotbad75@gmail.com>
2023-09-01 23:50:54 +0300

    add: new common errors

--- internal/errs/error.go
+++ internal/errs/error.go
@@ -6,4 +6,6 @@ var (
 	ErrNotFound            = errors.New("not found")
 	ErrInvalidUserId       = errors.New("invalid user id")
 	ErrInvalidExpireAtTime = errors.New("invalid expire at time")
+	ErrInvalidUserPercent  = errors.New("invalid user percent")
+	ErrNotEnoughUsers      = errors.New("not enough users in db")
 )
