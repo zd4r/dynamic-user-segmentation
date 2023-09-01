4e5bee712902e467739076fdc0090dc3d00e3d6f
zd4rova <fakenotbad75@gmail.com>
2023-09-01 23:52:00 +0300

    add: GetPercentOfAllUsers method

--- internal/service/user/get.go
+++ internal/service/user/get.go
@@ -2,10 +2,35 @@ package user
 
 import (
 	"context"
+	"math/rand"
+	"time"
 
+	"github.com/zd4r/dynamic-user-segmentation/internal/errs"
 	segmentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/segment"
+	userModel "github.com/zd4r/dynamic-user-segmentation/internal/model/user"
 )
 
 func (s *Service) GetSegments(ctx context.Context, userId int) ([]segmentModel.Segment, error) {
 	return s.userRepository.GetSegments(ctx, userId)
+}
+
+func (s *Service) GetPercentOfAllUsers(ctx context.Context, percent int) ([]userModel.User, error) {
+	users, err := s.userRepository.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	rand.New(rand.NewSource(time.Now().Unix()))
+	rand.Shuffle(len(users), func(i, j int) {
+		users[i], users[j] = users[j], users[i]
+	})
+
+	amount := percent * len(users) / 100
+	users = users[:amount]
+
+	if len(users) == 0 {
+		return nil, errs.ErrNotEnoughUsers
+	}
+
+	return users, nil
 }
