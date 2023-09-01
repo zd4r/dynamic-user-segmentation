09313aad84c3b4afe8bf175a97bb690eb76d9107
zd4rova <fakenotbad75@gmail.com>
2023-09-02 02:24:13 +0300

    add: expire_at db tag

--- internal/model/experiment/experiment.go
+++ internal/model/experiment/experiment.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Experiment struct {
-	UserId    int `db:"user_id"`
-	SegmentId int `db:"segment_id"`
-	ExpireAt  sql.NullTime
+	UserId    int          `db:"user_id"`
+	SegmentId int          `db:"segment_id"`
+	ExpireAt  sql.NullTime `db:"expire_at"`
 }
