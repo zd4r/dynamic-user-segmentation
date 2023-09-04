f1ea418fa932ddaa44d416645d5cb5fa4c77c713
zd4rova <fakenotbad75@gmail.com>
2023-09-04 21:00:30 +0300

    update: add GetUsersBySlug method

--- internal/service/segment/get.go
+++ internal/service/segment/get.go
@@ -4,8 +4,13 @@ import (
 	"context"
 
 	segmentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/segment"
+	userModel "github.com/zd4r/dynamic-user-segmentation/internal/model/user"
 )
 
 func (s *Service) GetBySlug(ctx context.Context, slug string) (*segmentModel.Segment, error) {
 	return s.segmentRepository.GetBySlug(ctx, slug)
+}
+
+func (s *Service) GetUsersBySlug(ctx context.Context, slug string) ([]userModel.User, error) {
+	return s.segmentRepository.GetUsersBySlug(ctx, slug)
 }
--- internal/service/segment/interfaces.go
+++ internal/service/segment/interfaces.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	segmentModel "github.com/zd4r/dynamic-user-segmentation/internal/model/segment"
+	userModel "github.com/zd4r/dynamic-user-segmentation/internal/model/user"
 	segmentRepo "github.com/zd4r/dynamic-user-segmentation/internal/repository/segment"
 )
 
@@ -12,6 +13,7 @@ type segmentRepository interface {
 	Create(ctx context.Context, slug string) error
 	DeleteBySlug(ctx context.Context, slug string) (int64, error)
 	GetBySlug(ctx context.Context, slug string) (*segmentModel.Segment, error)
+	GetUsersBySlug(ctx context.Context, slug string) ([]userModel.User, error)
 }
 
 var _ segmentRepository = (*segmentRepo.Repository)(nil)
